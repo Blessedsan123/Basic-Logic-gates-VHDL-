4c630a5a8825889f073fe39847cf0c0d9cfa82b5
Sandeep Panigrahi <156462298+Blessedsan123@users.noreply.github.com>
2024-12-13 19:12:22 +0530

    Add files via upload

---
+++ NOR Gate/design_norgate.vhd
@@ -0,0 +1,16 @@
+--Code your design here
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+
+entity norgate is
+port (in1 : in std_logic;
+      in2 : in std_logic;
+      output : out std_logic);
+      
+      end norgate;
+      
+architecture behavioral of norgate is
+begin
+output<=not(in1 or in2);
+end behavioral;
